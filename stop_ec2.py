da145d05f6279eaff42c6f8821807f980e0442b5
diegonorman <diegonorman5@gmail.com>
2025-05-30 14:07:14 -0300

    Ajusta stop_ec2.py para usar credenciais diretamente do ambiente

--- stop_ec2.py
+++ stop_ec2.py
@@ -1,21 +1,13 @@
-import argparse
 import boto3
 import os
 
 INSTANCE_IDS = ['i-06a4184ec28822e99']
 REGION = 'us-east-1'
 
-# Configurar argumentos de linha de comando
-parser = argparse.ArgumentParser(description='Parar instâncias EC2.')
-parser.add_argument('--aws-access-key-id', help='AWS Access Key ID')
-parser.add_argument('--aws-secret-access-key', help='AWS Secret Access Key')
-parser.add_argument('--aws-session-token', help='AWS Session Token')
-args = parser.parse_args()
-
-# Configurar cliente boto3 com credenciais temporárias
-aws_access_key_id = args.aws_access_key_id or os.getenv('AWS_ACCESS_KEY_ID')
-aws_secret_access_key = args.aws_secret_access_key or os.getenv('AWS_SECRET_ACCESS_KEY')
-aws_session_token = args.aws_session_token or os.getenv('AWS_SESSION_TOKEN')
+# Configurar cliente boto3 com credenciais temporárias diretamente do ambiente
+aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
+aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+aws_session_token = os.getenv('AWS_SESSION_TOKEN')
 
 # Verificar se todas as credenciais estão disponíveis
 if not aws_access_key_id:
